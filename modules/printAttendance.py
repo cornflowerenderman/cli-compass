86bafcb951982f11e9fbbd320a86818a215b0fbe
cornflowerenderman <cornflowerenderman6669@outlook.com>
2023-10-23 08:06:05 +1100

    Fix bug where incorrect colour was shown

--- modules/printAttendance.py
+++ modules/printAttendance.py
@@ -14,7 +14,7 @@ def printAttendance(attendance):
                 status=Fore.LIGHTGREEN_EX+Style.BRIGHT+status+Style.RESET_ALL
             elif(status!='Not Marked'):
                 status=Fore.LIGHTRED_EX+Style.BRIGHT+status+Style.RESET_ALL
-            elif(status=='Wellbeing') or ('Gen Edu'):
+            elif(status=='Wellbeing' or status=='Gen Edu'):
                 status=Fore.BLUE+Style.BRIGHT+status+Style.RESET_ALL
             temp.append(status)
         print("  "+(" - ".join(temp)))
