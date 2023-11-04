c3e0718d5edfde2b37553b9de1f4f970994cce7c
niforru <niforru@tutanota.com>
2023-11-05 09:28:18 +1100

    Create install script

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+# Incomplete but kinda working
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    include_package_data=True,
+    name='cli-compass',
+    version='0.1.0',
+    description='A command line interface for Compass Education',
+    packages=find_packages(),
+    url='https://github.com/cornflowerenderman/cli-compass',
+    license='MIT',
+    author='cornflowerenderman',
+    author_email='cornflowerenderman@duck.com',
+    python_requires='>=3.8.0',
+    install_requires=['pytz','colorama', 'browser-cookie3', 'beautifulsoup4']
+)
