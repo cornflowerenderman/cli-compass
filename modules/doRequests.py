b8f782785dc97ffb658392e43b9f8661f5182c84
niforru <niforru@tutanota.com>
2023-11-06 13:30:02 +1100

    Add colour coding for errors

--- modules/doRequests.py
+++ modules/doRequests.py
@@ -1,6 +1,7 @@
 # Centralised request functions, for simplicity and easier debugging
 
 import requests, json
+from colorama import Fore, Style
 
 headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0"}
 
@@ -21,39 +22,39 @@ def doRequest(isPost,isJson,url,cookies,payload):
                         else:
                             r = requests.get(url,cookies=cookies,headers=headers, timeout=timeout)
                     except requests.exceptions.HTTPError as error:
-                        print("[DEBUG] The server sent an invalid HTTP response")
+                        print(Fore.RED+"[DEBUG] The server sent an invalid HTTP response"+Fore.RESET)
                         raise requests.exceptions.HTTPError(error)
                 except requests.exceptions.SSLError as error:
-                    print("[DEBUG] SSL error, check system time and connection")
+                    print(Fore.RED+"[DEBUG] SSL error, check system time and connection"+Fore.RESET)
                     raise requests.exceptions.SSLError(error)
             except (requests.exceptions.ProxyError,requests.exceptions.InvalidProxyURL) as error:
-                print("[DEBUG] Proxy error, check proxy settings and try again")
+                print(Fore.RED+"[DEBUG] Proxy error, check proxy settings and try again"+Fore.RESET)
                 raise requests.exceptions.ProxyError(error)
         except requests.exceptions.Timeout as error:
-            print("[DEBUG] Timed out, check internet connection and proxy settings")
+            print(Fore.RED+"[DEBUG] Timed out, check internet connection and proxy settings"+Fore.RESET)
             raise requests.exceptions.Timeout(error)
     except requests.ConnectionError as error:
-        print("[DEBUG] Connection error, check internet connection, DNS and proxy settings")
+        print(Fore.RED+"[DEBUG] Connection error, check internet connection, DNS and proxy settings"+Fore.RESET)
         raise requests.ConnectionError(error)
     if(r.ok):
         if(isJson):
             try:
                 return json.loads(r.text)
             except json.decoder.JSONDecodeError as error:
-                print("[DEBUG] Error parsing JSON:")
+                print(Fore.RED+"[DEBUG] Error parsing JSON:"+Fore.RESET)
                 print(r.text)
                 raise json.decoder.JSONDecodeError(error)
         else:
             return r
     else:
         if(r.status_code == 500): #Likely expired/invalid ASP.NET_SessionId cookie
-            print("[DEBUG] Server sent HTTP Error 500, check ASP.NET_SessionId cookie")
-            raise requests.exceptions.HTTPError("HTTP Response 500")
+            print(Fore.RED+"[DEBUG] Server sent HTTP Error 500, check ASP.NET_SessionId cookie"+Fore.RESET)
+            raise requests.exceptions.HTTPError(Fore.RED+"HTTP Response 500"+Fore.RESET)
         elif(r.status_code == 403): #Likely bot detection, check User-Agent
-            print("[DEBUG] Server sent HTTP Error 403, likely Anti-bot detection, check User-Agent")
-            raise requests.exceptions.HTTPError("HTTP Response 403")
+            print(Fore.RED+"[DEBUG] Server sent HTTP Error 403, likely Anti-bot detection, check User-Agent"+Fore.RESET)
+            raise requests.exceptions.HTTPError(Fore.RED+"HTTP Response 403"+Fore.RESET)
         else: #Unknown response
-            print("[DEBUG] Server sent unknown HTTP response ("+str(r.status_code)+")")
+            print(Fore.RED+"[DEBUG] Server sent unknown HTTP response ("+str(r.status_code)+")"+Fore.RESET)
             raise requests.exceptions.HTTPError(r)
 
 def doPostRequest(url,cookies,payload):
